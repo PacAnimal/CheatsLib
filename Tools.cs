9e23950b4172f471beb4b1cfd763810071ad97bc
Nummer <vlad_tislenko@mail.ru>
2015-07-02 19:35:02 +0400

    Cheats Lib
    SRC

---
+++ Tools.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nummer.Cheats
+{
+    internal class Tools
+    {
+        public static byte[] StringToByteArray(string hex)
+        {
+            hex = hex.Replace(" ", "");
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+    }
+}
